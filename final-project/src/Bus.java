b0e7d0953d7ea6b8e89f3ea5a8f01de3fd17babe
Bruno Hortelão <bruno.hortelao@gmail.com>
2026-05-23 15:04:07 +0100

    +

--- final-project/src/Bus.java
+++ final-project/src/Bus.java
@@ -9,26 +9,39 @@ public class Bus {
     private int countPassengers;
     private int capacity;
     private BusStop currentBusStop;
+    BusLine busLine;
     private boolean direction; // if true the bus is going forward down the line, if false, it is returning
+    private int rear;
+    private int front;
 
     // CONSTRUCTORS
-    // construtor inicializa o ônibus com capacidade de 5 passageiros
+    // Default bus size is 5
     public Bus() {
         passengers = new Passenger[5];
         countPassengers = 0;
         capacity = 5;
+        busLine = null;
         currentBusStop = null;
+        direction = true;
+        front = 0;
+        rear = 0;
     }
 
-    // Se quisermos com tamanho ajustável
+    // A size can be set
     public Bus(int size) {
         passengers = new Passenger[size];
         countPassengers = 0;
         capacity = size;
+        busLine = null;
         currentBusStop = null;
+        direction = true;
     }
 
     // ACTIONS
+    public void setBusLine(BusLine newBusLine) {
+        busLine = newBusLine;
+        currentBusStop = newBusLine.root;
+    }
 
     public void board(Passenger passenger) {
         add(passenger);
@@ -36,17 +49,39 @@ public class Bus {
     }
 
     public void add(Passenger passenger) {
-        if (capacity == passengers.length) {
+
+        if (countPassengers == capacity) {
             System.out.println("ALERT: Bus is full!");
         } else {
-            passengers[countPassengers] = passenger;
+            // Guarda o passageiro na posição correta do fim
+            passengers[rear] = passenger;
+
+            // Avança o ponteiro 'rear' de forma circular
+            rear = (rear + 1) % passengers.length;
+
+            // Incrementa o total de pessoas a bordo
             countPassengers++;
         }
+    }
 
+
+    public Passenger remove() {
+        if (countPassengers == 0) {
+            System.out.println("Bus is empty!");
+            return null;
+        }
+
+        Passenger removed = passengers[front];
+        passengers[front] = null; 
+
+        // Avança a frente de forma circular
+        front = (front + 1) % passengers.length;
+        countPassengers--;
+
+        return removed;
     }
 
     public void disembark(int nDisembark) {
-        // Di
         for (int i = 0; i < nDisembark; i++) {
 
         }
@@ -54,6 +89,14 @@ public class Bus {
 
     // This method is used to make the bus advanced down the line
     public void advance() {
+        if (busLine == null || currentBusStop == null) {
+            System.out.println("ALERT: Either the BusLine hasn't been defined or it has no stops!");
+        } else {
+            advance(currentBusStop);
+        }
+    }
+
+    private void advance(BusStop current) {
         if (direction) { // Going forward(True)
             if (currentBusStop.next != null) {
                 currentBusStop = currentBusStop.next;
@@ -69,7 +112,7 @@ public class Bus {
                 currentBusStop = currentBusStop.previous;
             } else {
                 // The bus hits the end of the bus line and reverses the way back
-                direction = true; 
+                direction = true;
                 if (currentBusStop.next != null) {
                     currentBusStop = currentBusStop.next;
                 }
